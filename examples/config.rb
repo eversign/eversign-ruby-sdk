b681f9c0dcaab202da21ddeceb125324a9b5a7b7
Sachin Raka <Sachin@Sachins-MacBook-Pro.local>
2017-12-15 23:36:27 +0530

    Adding create_document as well as examples

--- examples/config.rb
+++ examples/config.rb
@@ -2,5 +2,6 @@ require 'eversign_client'
 
 EversignClient.configure do |c|
   c.api_base = 'https://api.eversign.com/api'
-  c.access_key = '5aefd3ce16673d7b1d4b703693d3c82f'
+  c.access_key = '<YOUR_ACCESS_KEY>'
 end
+
